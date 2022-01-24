db6cd290d33d22a2cec6ebe6761ebdcc6c6c73c0
Josh Goldstein <josh.goldstein@autodesk.com>
2022-01-24 12:45:44 -0700

    Adding selection count info card

--- ManageAttributes_Web.js
+++ ManageAttributes_Web.js
@@ -2,8 +2,12 @@ window.ManageAttributes = window.ManageAttributes || {};
 
 /*** web/UI code - runs natively in the plugin process ***/
 
+// similar to Properties Plus, specify a max amount of objects the UI responds to
+ManageAttributes.nMaxObjectCount = 1000;
+
 // elements that get updated on selection and context change
 ManageAttributes.editingContextInfoCard = undefined;
+ManageAttributes.selectionCountInfoCard = undefined;
 
 // initialize the UI
 ManageAttributes.initializeUI = function()
@@ -23,7 +27,7 @@ ManageAttributes.initializeUI = function()
     contentContainer.appendChild(document.createElement('p'));
 
     // editing context section
-    let currentHistoryAttributesHeader = new FormIt.PluginUI.HeaderModule('In Current History', '', 'headerContainer');
+    let currentHistoryAttributesHeader = new FormIt.PluginUI.HeaderModule('In Editing Context', '', 'headerContainer');
     contentContainer.appendChild(currentHistoryAttributesHeader.element);
 
     // context properties info card
@@ -38,6 +42,12 @@ ManageAttributes.initializeUI = function()
     let selectedObjectAttributesHeader = new FormIt.PluginUI.HeaderModule('On Selected Object', '', 'headerContainer');
     contentContainer.appendChild(selectedObjectAttributesHeader.element);
 
+    // selection count info card
+    ManageAttributes.selectionCountInfoCard = new FormIt.PluginUI.SelectionCountInfoCard(ManageAttributes.nMaxObjectCount);
+    contentContainer.appendChild(ManageAttributes.selectionCountInfoCard.element);
+    // append the too many objects div now that it has a parent
+    ManageAttributes.selectionCountInfoCard.appendTooManyObjectsMessage();
+
     // create the footer
     document.body.appendChild(new FormIt.PluginUI.FooterModule().element);
 }
@@ -51,6 +61,7 @@ ManageAttributes.updateUI = function()
         let currentSelectionInfo = JSON.parse(result);
 
         ManageAttributes.editingContextInfoCard.update(currentSelectionInfo);
+        ManageAttributes.selectionCountInfoCard.update(currentSelectionInfo);
     });
 
 
